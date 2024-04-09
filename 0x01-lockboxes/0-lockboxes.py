c71124e332e77694cce4ba7521bb07ce0e25cb09
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-09 02:45:06 -0700

    code documented

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,11 +2,26 @@
 
 
 def canUnlockAll(boxes):
+    """
+    Checks if all boxes can be unlocked given the keys inside them.
+
+    Args:
+    - boxes: A list of lists representing the boxes and their keys.
+
+    Returns:
+    - True if all boxes can be unlocked, False otherwise.
+    """
     num_boxes = len(boxes)
 
     visited = set()
 
     def unlock_boxes(box_index):
+        """
+        Recursively unlocks boxes starting from the given box index.
+
+        Args:
+        - box_index: Index of the box to start unlocking from.
+        """
         visited.add(box_index)
         keys = boxes[box_index]
 
