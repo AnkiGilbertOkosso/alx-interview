acc8088f91c694b0cb03b27d4e36ba7b181c28ca
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-09 03:06:30 -0700

    l

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,18 +12,28 @@ def canUnlockAll(boxes):
     Returns:
     - True if all boxes can be unlocked, False otherwise.
     """
+
     num_boxes = len(boxes)
 
     visited = set()
-    satck[0]
 
-    while stack:
-        box_index = stack.pop()
+    def unlock_boxes(box_index):
+        """
+        Recursively unlocks boxes starting from the given box index.
+
+        Args:
+        - box_index: Index of the box to start unlocking from.
+        """
         visited.add(box_index)
-
         keys = boxes[box_index]
 
         for key in keys:
             if key not in visited and key < num_boxes:
-                stack.append(key)
+                unlock_boxes(key)
+
+        if len(visited) == num_boxes:
+            return
+
+    unlock_boxes(0)
+
     return len(visited) == num_boxes
