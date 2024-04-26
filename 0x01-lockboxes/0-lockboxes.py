9ce4fbffa399bd6d21c0bd98b52df4c0676cbe29
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-26 06:50:15 +0100

    task 1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,39 +1,25 @@
 #!/usr/bin/python3
-"""Lockboxes"""
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
-    """
-    Checks if all boxes can be unlocked given the keys inside them.
+    '''Determines whether all boxes in a list of boxes, each containing keys
+    to other boxes, can be unlocked, assuming the first box is unlocked.
 
-    Args:
-    - boxes: A list of lists representing the boxes and their keys.
-
-    Returns:
-    - True if all boxes can be unlocked, False otherwise.
-    """
-
-    num_boxes = len(boxes)
-
-    visited = set()
-
-    def unlock_boxes(box_index):
-        """
-        Recursively unlocks boxes starting from the given box index.
-
-        Args:
-        - box_index: Index of the box to start unlocking from.
-        """
-        visited.add(box_index)
-        keys = boxes[box_index]
-
-        for key in keys:
-            if key not in visited and key < num_boxes:
-                unlock_boxes(key)
-
-        if len(visited) == num_boxes:
-            return
-
-    unlock_boxes(0)
-
-    return len(visited) == num_boxes
+    :param boxes: A list of lists representing the boxes and their keys.
+    :type boxes: list[list[int]]
+    :return: True if all boxes can be unlocked, False otherwise.
+    :rtype: bool
+    '''
+    n = len(boxes)
+    visited_boxes = set([0])
+    unvisited_boxes = set(boxes[0]).difference(set([0]))
+    while len(unvisited_boxes) > 0:
+        box_idx = unvisited_boxes.pop()
+        if not box_idx or box_idx >= n or box_idx < 0:
+            continue
+        if box_idx not in visited_boxes:
+            unvisited_boxes = unvisited_boxes.union(boxes[box_idx])
+            visited_boxes.add(box_idx)
+    return n == len(visited_boxes)
