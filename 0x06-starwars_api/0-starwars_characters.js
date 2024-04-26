8158783aea19f13e9c144e2398aa3c47496d5585
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-26 08:29:22 +0100

    fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,33 +6,33 @@ const API_URL = 'https://swapi-api.hbtn.io/api';
  * Fetches and prints all characters of a Star Wars movie.
  * @param {number} movieId - The ID of the Star Wars movie.
  */
-function printMovieCharacters(movieId) {
-    request(`${API_URL}/films/${movieId}/`, (err, _, body) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        const charactersURLs = JSON.parse(body).characters;
-        const characterNamesPromises = charactersURLs.map(
-            url => new Promise((resolve, reject) => {
-                request(url, (promiseErr, __, charactersReqBody) => {
-                    if (promiseErr) {
-                        reject(promiseErr);
-                    }
-                    resolve(JSON.parse(charactersReqBody).name);
-                });
-            }));
+function printMovieCharacters (movieId) {
+  request(`${API_URL}/films/${movieId}/`, (err, _, body) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    const charactersURLs = JSON.parse(body).characters;
+    const characterNamesPromises = charactersURLs.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-        Promise.all(characterNamesPromises)
-            .then(names => console.log(names.join('\n')))
-            .catch(allErr => console.error(allErr));
-    });
+    Promise.all(characterNamesPromises)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.error(allErr));
+  });
 }
 
 const movieId = process.argv[2];
 if (!movieId || isNaN(movieId)) {
-    console.error('Usage: ./0-starwars_characters.js <movie_id>');
-    process.exit(1);
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
 }
 
 printMovieCharacters(movieId);
